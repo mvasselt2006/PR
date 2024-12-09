7924921941a443b258ed52419b17d9213c19ae6c
Marloes <m.van.asselt.2@student.rug.nl>
2024-12-09 11:06:53 +0100

    epoch input toegevoegd

--- Lab2/ResNet18_cifar10_lab2/ResNet18_cifar10_lab2/main.py
+++ Lab2/ResNet18_cifar10_lab2/ResNet18_cifar10_lab2/main.py
@@ -20,6 +20,7 @@ parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
+parser.add_argument('--epochs','-e',default=50, type=int, help='Total amount of epochs' )
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -32,23 +33,23 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
 
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
@@ -137,8 +138,8 @@ def test(epoch):
         best_acc = acc
 
 if __name__ == "__main__":
-
-    for epoch in range(start_epoch, start_epoch+100):
+    tot_epoch= args.epochs
+    for epoch in range(start_epoch, tot_epoch-start_epoch):
         train(epoch)
         test(epoch)
         #scheduler.step()
