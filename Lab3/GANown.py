6c3ff277905a87745770a52522a97cfd19d5f8a9
elsjan <martheelsjan@gmail.com>
2025-01-29 14:13:30 +0100

    DIT ZIJN WEL DE GOEDE BESTANDEN!

---
+++ Lab3/GANown.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torchvision.datasets as datasets
+from torchvision.utils import save_image
+import torch.nn.functional as F
+
+# hyper-parameters configurations
+image_size = 32 # we need to resize image to 32X32
+batch_size = 128
+nz = 100 # latent vector size
+beta1 = 0.5 # beta1 value for Adam optimizer
+lr = 0.0001 # learning rate
+sample_size = 32 # fixed sample size
+epochs = 30 # number of epoch to train
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# image transformations
+transform = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+    (0.5, 0.5, 0.5)),
+])
+
+# dataset
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+
+# generator
+class Generator(nn.Module):
+    def __init__(self, nz):
+        super(Generator, self).__init__()
+        self.nz = nz # the input noise vector
+
+        # the rest of the codes to be filled ...
+        self.conv1 = nn.ConvTranspose2d() #in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, groups=1, bias=True, dilation=1, padding_mode='zeros', device=None, dtype=None)
+        # first tries
+    def forward(self):
+        F.softmax() #only know this should be the final activation function 
+
+# discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        
+        # the rest of the codes to be filled ...
+
+
+# train the network
+
+# initialize models
+generator = Generator(nz).to(device)
+discriminator = Discriminator().to(device)
+
+# optimizers
+optim_g = optim.Adam(generator.parameters(), lr=lr, betas=(beta1, 0.999))
+optim_d = optim.Adam(discriminator.parameters(), lr=lr, betas=(beta1, 0.999))
+
+# loss function
+criterion = nn.BCELoss()
+
+# to store the generator and discriminator loss after each epoch
+losses_g = []
+losses_d = []
+
+# creat the noise signal as input to the generator
+def create_noise(sample_size,nz):
+  return torch.randn(sample_size,nz,1,1).to(device)
+
+# function to train the discriminator network
+def train_discriminator(optimizer, data_real, data_fake):
+    b_size = data_real.size(0)
+    # get the real label vector
+    real_label = torch.ones(b_size, 1).to(device).squeeze()
+    # get the fake label vector
+    fake_label = torch.zeros(b_size, 1).to(device).squeeze()
+    optimizer.zero_grad()
+    # get the outputs by doing real data forward pass
+    output_real = discriminator(data_real).view(-1)
+    #print(output_real.size())
+    loss_real = criterion(output_real, real_label)
+    # get the outputs by doing fake data forward pass
+    output_fake = discriminator(data_fake).view(-1)
+    loss_fake = criterion(output_fake, fake_label)
+    # compute gradients of real loss
+    loss_real.backward()
+    # compute gradients of fake loss
+    loss_fake.backward()
+    # update discriminator parameters
+    optimizer.step()
+    return loss_real + loss_fake
+
+
+# function to train the generator network
+def train_generator(optimizer, data_fake):
+    b_size = data_fake.size(0)
+    # get the real label vector
+    real_label = torch.ones(b_size, 1).to(device).squeeze()
+    optimizer.zero_grad()
+    # output by doing a forward pass of the fake data through discriminator
+    output = discriminator(data_fake).view(-1)
+    loss = criterion(output, real_label)
+    # compute gradients of loss
+    loss.backward()
+    # update generator parameters
+    optimizer.step()
+    return loss
+
+# create the noise vector
+noise = create_noise(sample_size, nz)
+
+for epoch in range(epochs):
+    loss_g = 0.0
+    loss_d = 0.0
+    for batch_idx, data in enumerate(trainloader):
+        image, _ = data
+        image = image.to(device)
+        b_size = len(image)
+        # forward pass through generator to create fake data
+        data_fake = generator(create_noise(b_size, nz)).detach()
+        data_real = image
+        loss_d += train_discriminator(optim_d, data_real, data_fake)
+        data_fake = generator(create_noise(b_size, nz))
+        loss_g += train_generator(optim_g, data_fake)
+    # final forward pass through generator to create fake data...
+    # ...after training for current epoch
+    generated_img = generator(noise).cpu().detach()
+    # save the generated torch tensor models to disk
+    save_image(generated_img, f"outputs/gen_img{epoch}.png", normalize=True)
+    epoch_loss_g = loss_g / batch_idx # total generator loss for the epoch
+    epoch_loss_d = loss_d / batch_idx # total discriminator loss for the epoch
+    losses_g.append(epoch_loss_g)
+    losses_d.append(epoch_loss_d)
+    print(f"Epoch {epoch+1} of {epochs}")
+    print(f"Generator loss: {epoch_loss_g:.8f}, Discriminator loss: {epoch_loss_d:.8f}")
