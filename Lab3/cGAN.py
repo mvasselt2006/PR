24ba8777049acb2db95aeed821ff249ab8c8ed93
Marloes <m.van.asselt.2@student.rug.nl>
2025-02-03 14:38:41 +0100

    werkt nu hopelijk

--- Lab3/cGAN.py
+++ Lab3/cGAN.py
@@ -16,6 +16,7 @@ beta1 = 0.5 # beta1 value for Adam optimizer
 lr = 0.0001 # learning rate  # in paper 0.0002 is used
 sample_size = 32 # fixed sample size
 epochs = 30 # number of epoch to train
+# label_in= torch.zeros(10)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # image transformations
@@ -40,7 +41,7 @@ trainloader = torch.utils.data.DataLoader(
 class Generator(nn.Module):
     def __init__(self, nz):
         super(Generator, self).__init__()
-        self.nz = nz # the input noise vector
+        self.nz = nz+10 # the input noise vector
 
         # the rest of the codes to be filled ...
 
@@ -59,7 +60,7 @@ class Generator(nn.Module):
                 )
             
         self.model = nn.Sequential(
-            block(nz, 128, norm=False, stride=1, padding=0), 
+            block(self.nz, 128, norm=False, stride=1, padding=0), 
             block(128, 64),
             block(64, 32),
             nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1, bias=False),
@@ -99,7 +100,7 @@ class Generator(nn.Module):
 
 # discriminator
 class Discriminator(nn.Module):
-    def __init__(self,labels):
+    def __init__(self):
         super(Discriminator, self).__init__()
         
         # the rest of the codes to be filled ...
@@ -121,12 +122,19 @@ class Discriminator(nn.Module):
             block(3, 32, norm=False),
             block(32, 64),
             block(64, 128),
-            nn.Conv2d(128, 1, kernel_size=4, stride=1, padding=0, bias=False),  #have to look into why stride and padding this way
-            nn.Sigmoid()
+            nn.Conv2d(128, 1, kernel_size=4, stride=1, padding=0, bias=False)
         )
 
-    def forward(self, image):
-        return self.model(image)   #see some things about flattening the image, might be needed here
+        #Define linear operation for final classification
+        self.fc1 = nn.Linear(11, 1)
+
+    def forward(self, image,labels):
+        image = self.model(image)
+        image_cat= torch.cat((image, labels),axis=1)
+        x = torch.flatten(image_cat, 1) # flatten all dimensions except batch
+        x = F.sigmoid(self.fc1(x))
+
+        return x  #see some things about flattening the image, might be needed here
 
 
 # train the network
@@ -150,7 +158,7 @@ losses_d = []
 def create_noise(sample_size,nz):
   return torch.randn(sample_size,nz,1,1).to(device)
 def create_label(sample_size):
-  labels =torch.randint(low=0,high=9,size=sample_size).to(device)
+  labels =torch.randint(low=0,high=9,size=(sample_size,1)).to(device)
   label_vector= F.one_hot(labels,num_classes=10)
   return label_vector
 
@@ -197,6 +205,9 @@ def train_generator(optimizer, data_fake,labels):
 if __name__ == "__main__":
     # create the noise vector
     noise = create_noise(sample_size, nz)
+    label_sample= create_label(sample_size)
+    sample_vector= torch.reshape(label_sample.detach().clone(),[sample_size,10,1,1])
+    sample_noise= torch.cat((noise,sample_vector),1) #Creates [128,110,1,1] noise vector
 
     for epoch in range(epochs):
         loss_g = 0.0
@@ -210,7 +221,7 @@ if __name__ == "__main__":
             #Change to one-hot
             label_vector= F.one_hot(label,num_classes=10)
             #Add empty dimensions to concat with noise vector of [128,100,1,1] shape
-            shaped_vector = torch.reshape(label_vector.detach().clone(),[128,10,1,1])
+            shaped_vector = torch.reshape(label_vector.detach().clone(),[b_size,10,1,1])
             
             #generate input noise
             input_noise=create_noise(b_size, nz)
@@ -219,19 +230,19 @@ if __name__ == "__main__":
             # forward pass through generator to create fake data
             data_fake = generator(label_noise).detach()
             data_real = image
-            loss_d += train_discriminator(optim_d, data_real, data_fake,label_vector)
+            loss_d += train_discriminator(optim_d, data_real, data_fake,shaped_vector)
 
             #generate input noise and label for training generator
             input_noise=create_noise(b_size, nz)
             label_vector= create_label(b_size)
-            shape_vector= torch.reshape(label_vector.detach().clone(),[b_size,10,1,1])
+            shaped_vector= torch.reshape(label_vector.detach().clone(),[b_size,10,1,1])
             label_noise= torch.cat((input_noise,shaped_vector),1) #Creates [128,110,1,1] noise vector
 
             data_fake = generator(label_noise)
-            loss_g += train_generator(optim_g, data_fake,label_vector)
+            loss_g += train_generator(optim_g, data_fake,shaped_vector)
         # final forward pass through generator to create fake data...
         # ...after training for current epoch
-        generated_img = generator(noise).cpu().detach()
+        generated_img = generator(sample_noise).cpu().detach()
         # save the generated torch tensor models to disk
         save_image(generated_img, f"outputs/gen_img{epoch}.png", normalize=True)           # so in this folder the images can be viewed during running
         epoch_loss_g = loss_g / batch_idx # total generator loss for the epoch
