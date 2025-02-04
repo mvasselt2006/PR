66055dfb51b3bd83f8bbcb3749f6f8f17de0c22d
Marloes <m.van.asselt.2@student.rug.nl>
2025-02-04 14:30:27 +0100

    run dingetjes aangepast

--- Lab3/cGAN.py
+++ Lab3/cGAN.py
@@ -15,7 +15,7 @@ nz = 100 # latent vector size
 beta1 = 0.5 # beta1 value for Adam optimizer
 lr = 0.0001 # learning rate  # in paper 0.0002 is used
 sample_size = 32 # fixed sample size
-epochs = 30 # number of epoch to train
+epochs = 50 # number of epoch to train
 # label_in= torch.zeros(10)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -245,7 +245,7 @@ if __name__ == "__main__":
         # ...after training for current epoch
         generated_img = generator(sample_noise).cpu().detach()
         # save the generated torch tensor models to disk
-        save_image(generated_img, f"outputs/gen_img{epoch}.png", normalize=True)           # so in this folder the images can be viewed during running
+        save_image(generated_img, f"cOutputs/gen_img{epoch}.png", normalize=True)           # so in this folder the images can be viewed during running
         epoch_loss_g = loss_g / batch_idx # total generator loss for the epoch
         epoch_loss_d = loss_d / batch_idx # total discriminator loss for the epoch
         losses_g.append(epoch_loss_g.cpu().detach().numpy())
@@ -267,4 +267,4 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
-    plt.savefig("outputs/loss.png")
+    plt.savefig("cOutputs/loss.png")
